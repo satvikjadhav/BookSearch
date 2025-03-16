722ae5397e2633d5f2e373fa28dd62d9911c0b04
satvikjadhav <satvikj5@gmail.com>
2025-03-16 16:06:46 -0500

    Initial Commit

---
+++ BookSearch/BookSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchApp.swift
+//  BookSearch
+//
+//  Created by Satvik  Jadhav on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
